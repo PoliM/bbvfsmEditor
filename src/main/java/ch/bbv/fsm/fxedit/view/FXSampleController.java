dc1fcd1e1085108debbe21452224b957a6e28dfb
Lukas Weibel <lukas.weibel@bbv.ch>
2012-05-23 20:48:55 +0200

    Working on sample.

---
+++ src/main/java/ch/bbv/fsm/fxedit/view/FXSampleController.java
@@ -0,0 +1,9 @@
+package ch.bbv.fsm.fxedit.view;
+
+import javafx.event.ActionEvent;
+
+public class FXSampleController {
+	public void ggg(final ActionEvent event) {
+		System.out.println("You clicked me!");
+	}
+}
