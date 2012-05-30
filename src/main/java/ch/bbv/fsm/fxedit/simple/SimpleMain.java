597256077c6107dce6038a5ff06eef825db62dd1
PoliM <marco.a.poli@gmail.com>
2012-05-30 20:56:24 +0200

    Simple stuff

---
+++ src/main/java/ch/bbv/fsm/fxedit/simple/SimpleMain.java
@@ -0,0 +1,13 @@
+package ch.bbv.fsm.fxedit.simple;
+
+public class SimpleMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("One day this will start something awesome!");
+
+	}
+
+}
