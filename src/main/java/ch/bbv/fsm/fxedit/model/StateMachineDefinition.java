e40f180f6468cab4f1d12cec7183e58c6fd74820
PoliM <marco.a.poli@gmail.com>
2012-05-30 20:15:31 +0200

    Undo

--- src/main/java/ch/bbv/fsm/fxedit/model/StateMachineDefinition.java
+++ src/main/java/ch/bbv/fsm/fxedit/model/StateMachineDefinition.java
@@ -2,35 +2,52 @@ package ch.bbv.fsm.fxedit.model;
 
 import java.util.List;
 
-import javafx.beans.property.SimpleObjectProperty;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 
 @ToString
-public class StateMachineDefinition extends Nameable {
+public class StateMachineDefinition {
 
-	private final ObservableList<State> states = FXCollections.observableArrayList();
+	@Getter
+	@Setter
+	private String name;
 
-	private final SimpleObjectProperty<State> mainState = new SimpleObjectProperty<State>(this, "mainState");
+	private final List<State> states = Lists.newLinkedList();
+
+	@Getter
+	@Setter
+	private State mainState;
 
 	private final List<Event> events = Lists.newLinkedList();
 
 	@Getter
 	private final StateMachine stateMachine = new StateMachine();
 
-	public ObservableList<State> getStates() {
-		return states;
+	public void addState(final State state) {
+		states.add(state);
 	}
 
-	public State getMainState() {
-		return mainState.get();
+	public void removeState(final State state) {
+		states.remove(state);
 	}
 
-	public void setMainState(final State state) {
-		mainState.set(state);
+	public ImmutableList<State> getStates() {
+		return ImmutableList.copyOf(states);
+	}
+
+	public void addEvent(final Event event) {
+		events.add(event);
+	}
+
+	public void removeEvent(final Event event) {
+		events.remove(event);
+	}
+
+	public ImmutableList<Event> getEvents() {
+		return ImmutableList.copyOf(events);
 	}
 }
