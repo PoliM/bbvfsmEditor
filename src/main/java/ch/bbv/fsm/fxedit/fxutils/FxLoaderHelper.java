3c2ffecd7e0dad24d8936854466ea3b839282ed6
Lukas Weibel <lukas.weibel@bbv.ch>
2012-05-30 21:56:26 +0200

    Added about box.

--- src/main/java/ch/bbv/fsm/fxedit/fxutils/FxLoaderHelper.java
+++ src/main/java/ch/bbv/fsm/fxedit/fxutils/FxLoaderHelper.java
@@ -42,4 +42,5 @@ public class FxLoaderHelper {
 			// Nothing to do here
 		}
 	}
+
 }
--- src/main/java/ch/bbv/fsm/fxedit/view/application/FsmEditorApplicationController.java
+++ src/main/java/ch/bbv/fsm/fxedit/view/application/FsmEditorApplicationController.java
@@ -1,12 +1,23 @@
 package ch.bbv.fsm.fxedit.view.application;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import javax.inject.Inject;
+
+import ch.bbv.fsm.fxedit.fxutils.FxLoaderHelper;
 
 import com.google.common.eventbus.EventBus;
 
@@ -14,9 +25,16 @@ public class FsmEditorApplicationController implements Initializable {
 
 	private final EventBus eventBus;
 
+	@FXML
+	private AnchorPane anchorPane;
+
+	private final FxLoaderHelper fxLoaderHelper;
+
 	@Inject
-	public FsmEditorApplicationController(final EventBus eventBus) {
+	public FsmEditorApplicationController(final EventBus eventBus,
+			final FxLoaderHelper fxLoaderHelper) {
 		this.eventBus = eventBus;
+		this.fxLoaderHelper = fxLoaderHelper;
 	}
 
 	@Override
@@ -28,5 +46,36 @@ public class FsmEditorApplicationController implements Initializable {
 	@FXML
 	protected void close() {
 		eventBus.post("gnap");
+	}
+
+	@FXML
+	protected void about(final ActionEvent event) {
+		final Stage stage = new Stage(StageStyle.TRANSPARENT);
+		stage.addEventHandler(MouseEvent.MOUSE_CLICKED,
+				new EventHandler<MouseEvent>() {
+
+					@Override
+					public void handle(final MouseEvent arg0) {
+						stage.hide();
+					}
+				});
+
+		AnchorPane aboutContent = null;
+		try {
+			aboutContent = fxLoaderHelper.loadFXML(getClass().getResource(
+					"about.fxml"));
+		} catch (final IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		final Scene scene1 = new Scene(aboutContent);
+		stage.initModality(Modality.APPLICATION_MODAL);
+		// also tried WINDOW_MODAL
+		stage.initOwner(anchorPane.getScene().getWindow());
+		stage.setScene(scene1);
+		stage.show();
+		// Note that the next line is executed immediately, before the dialog is
+		// hidden
 	}
 }
